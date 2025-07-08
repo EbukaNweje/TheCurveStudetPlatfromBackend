ea65e3f21a523737c57e6801d7d1357119b8317b
ColdDdev <colindecorce@gmail.com>
2025-07-08 15:16:32 +0100

    Working

--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,21 @@ const User = require("../models/Users");
 
 exports.getOneUser = async (req, res) => {
     try {
-        const user = await User.findOne({email:req.body.email}).populate("assessment");
+        const { email } = req.query; 
+
+        if (!email) {
+            return res.status(400).json({ message: "Email is required" });
+        }
+
+        const user = await User.findOne({ email }).populate("assessment");
+
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
+
         return res.status(200).json(user);
     } catch (err) {
         console.error("Error fetching user:", err);
-        return res.status(500).send({ message: "Server error", error: err });
+        return res.status(500).send({ message: "Server error", error: err.message });
     }
 };
